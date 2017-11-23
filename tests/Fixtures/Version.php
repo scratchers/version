780fd6147f7b12b98c039a1b85cfbc2ad29c8905
Jeff Puckett <jeff@jeffpuckett.com>
2017-11-23 04:22:17 +0000

    test add Fixtures\Version

---
+++ tests/Fixtures/Version.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace jpuck\version\tests\Fixtures;
+
+class Version
+{
+    const VERSION = '1.0.0';
+}
--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace jpuck\version\tests;
+
 use PHPUnit\Framework\TestCase;
 use jpuck\version\Version;
 
@@ -8,5 +10,10 @@ class VersionTest extends TestCase
     public function test_can_instantiate_object()
     {
         $this->assertInstanceOf(Version::class, new Version);
+    }
+
+    public function test_can_get_version_number()
+    {
+        $this->assertSame('1.0.0', Fixtures\Version::VERSION);
     }
 }
